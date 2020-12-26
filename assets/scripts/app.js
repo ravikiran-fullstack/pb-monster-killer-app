ec38093fa15bc528940bb8b3ee3434d99f6412ca
kiran-code <ravikiransjce.code@gmail.com>
2020-12-26 22:31:13 +0530

    Working day and night theme for the page

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -87,3 +87,13 @@ function healPlayerHandler(){
 attackBtn.addEventListener('click', attackMonsterHandler);
 strongAttackBtn.addEventListener('click', strongAttackMonsterHandler);
 healBtn.addEventListener('click', healPlayerHandler);
+
+
+
+const btn = document.querySelector(".btn-toggle");
+
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle("dark-theme");
+});
